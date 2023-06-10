26a33fdf884b005c580e11ae6d17b3d4dc4664a9
alipdeveloper <lostyoungsters@gmail.com>
2023-06-10 14:20:17 +0700

    Add about page

---
+++ src/scripts/views/pages/about.js
@@ -0,0 +1,16 @@
+const About = {
+  async render() {
+    return `
+    <resto-detail>
+      <h2>
+      <span class="title-content">Detail Restaurant</span>
+      </h2>
+
+      <div id="resto-detail" class="row"></div>
+      <div id="likeButtonContainer"></div>
+    </resto-detail>
+    `;
+  },
+};
+
+export default About;
