399a4876b70f65c95e5eca3ad8b53ce2a57688f4
ardanu01 <ardanubroto@gmail.com>
2023-06-12 09:32:02 +0800

    edit by ardanu

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,12 @@
 import '../styles/login.css';
 import '../styles/main.css';
 import '../styles/signup.css';
+import '../public/hero/hero-1.jpg'
+import '../public/logo/back-icon.svg';
+import '../public/logo/logo-2.png';
+import '../public/logo/logo_byetrash.svg';
+import '../public/pict/profile.jpg';
+import '../public/pict/thumb1.png';
 import 'regenerator-runtime';
 import app from './views/app';
 
