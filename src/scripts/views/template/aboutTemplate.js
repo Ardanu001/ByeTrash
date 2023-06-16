845cc39da784119002795d32c7bdf33c49383720
Fadhil <fadhilahmad264@gmail.com>
2023-06-16 21:45:59 +0700

    profile
    	new file:   src/public/pict/profile4.jpg
    	modified:   src/scripts/views/template/aboutTemplate.js

--- src/scripts/views/template/aboutTemplate.js
+++ src/scripts/views/template/aboutTemplate.js
@@ -65,7 +65,7 @@ const aboutTemplate = () => `
 
         <div class="team-card">
           <img
-          src="https://github.com/Ardanu001/ByeTrash/blob/main/src/public/pict/profile.jpg?raw=true"
+          src="https://github.com/Ardanu001/ByeTrash/blob/main/src/public/pict/profile4.jpg?raw=true"
           class="img-fluid"
           alt="Team Member 4" 
           />
