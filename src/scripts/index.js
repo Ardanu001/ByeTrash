b891f88d2cd168286ad407fb06b2c8496c9bfcd6
alipdeveloper <lostyoungsters@gmail.com>
2023-06-16 13:09:44 +0700

    Add preloader

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,17 +1,26 @@
-/* eslint-disable new-cap */
-import '../styles/main.css';
-import 'regenerator-runtime';
-import app from './views/app';
+import "../styles/main.css";
+import "regenerator-runtime";
+import app from "./views/app";
 
 const App = new app({
-  content: document.querySelector('main'),
-  button: document.querySelector('.navbar-toggler'),
+  content: document.querySelector("main"),
+  button: document.querySelector(".navbar-toggler"),
 });
 
-window.addEventListener('hashchange', () => {
+window.addEventListener("hashchange", () => {
   App.renderpage();
 });
 
-window.addEventListener('load', () => {
+window.addEventListener("load", () => {
   App.renderpage();
+});
+
+window.addEventListener("load", () => {
+  const loader = document.querySelector(".loader");
+
+  loader.classList.add("loader-hidden");
+
+  loader.addEventListener("transitioned", () => {
+    document.body.removeChild("loader");
+  });
 });
