8e1c5a987ad5e4a58284ea3045fadb5c4dd04d7b
Fadhil <fadhilahmad264@gmail.com>
2023-06-16 20:31:15 +0700

    edit by fadhil

--- src/scripts/views/template/homeTemplate.js
+++ src/scripts/views/template/homeTemplate.js
@@ -71,24 +71,73 @@ const homeTemplate = () => `<!-- Hero -->
 
 <!-- Panduan website end -->
 
-<!--Google map-->
-<section id="maps" style="background-color: #fff">
-  <div class="container pt-5 pb-5">
-    <div class="row text-center mb-3">
-      <div class="col fs-4">
-        <h2>Maps</h2>
+<!--Artikel-->
+
+<section class="pt-5 pb-4 text-center container">
+    <div class="row py-3">
+      <div class="col-lg-6 col-md-8 mx-auto">
+        <h1 class="fw-bold">Artikel</h1>
+        <p class="lead text-body-secondary"></p>
       </div>
     </div>
-    <div id="map-container-google-1" class="z-depth-1-half map-container">
-      <iframe
-        src="https://maps.google.com/maps?q=manhatan&t=&z=13&ie=UTF8&iwloc=&output=embed"
-        frameborder="0"
-        style="height: 100%; width: 100%"
-        allowfullscreen
-      ></iframe>
-    </div>
-  </div>
 </section>
-<!--Google Maps end-->`;
+
+<div class="album">
+    <div class="container">
+    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+
+        <div class="col d-flex">
+          <div class="card shadow-sm h-100 d-flex flex-column">
+            <img class="bd-placeholder-img card-img-top img-blogs img-fluid" src="./pict/thumb2.jpg" width="100%" height="225" alt="Placeholder: Thumbnail" />
+                <title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/>
+            <div class="card-body">
+              <p class="card-text fw-bold">Indonesia Memiliki Masalah Sampah Yang Serius</p>
+              <p class="card-text">Sampah kini menjadi salah satu polemik permasalahan yang terjadi di Indonesia.</p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                    <a class="btn btn-outline-dark btn-sm" href="https://maritimefairtrade.org/indonesia-memiliki-masalah-sampah-yang-serius/" target="_blank" role="button">Read</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        
+        <div class="col">
+            <div class="card shadow-sm h-100 d-flex flex-column">
+              <img class="bd-placeholder-img card-img-top img-blogs img-fluid" src="./pict/thumb3.jpg" width="100%" height="225" alt="Placeholder: Thumbnail" />
+                  <title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/>
+              <div class="card-body">
+                <p class="card-text fw-bold">Menengok Layanan Pengolahan Sampah di Indonesia, Sudahkah Optimal? </p>
+                <p class="card-text">Saat ini, pengelolaan sampah di Indonesia dinilai masih sangat kurang efektif. Timbulan sampah menumpuk menimbulkan banyak masalah baru.</p>
+                <div class="d-flex justify-content-between align-items-center">
+                  <div class="btn-group">
+                    <a class="btn btn-outline-dark btn-sm" href="#" target="_blank" role="button">Read</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col d-flex">
+            <div class="card shadow-sm h-100 d-flex flex-column">
+              <img class="bd-placeholder-img card-img-top img-blogs img-fluid" src="./pict/thumb4.jpg" width="100%" height="225" alt="Placeholder: Thumbnail" />
+                  <title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/>
+              <div class="card-body">
+                <p class="card-text fw-bold">Cara Membuat Kompos dari Sampah Rumah Tangga Tanpa Bau</p>
+                <p class="card-text">Kompos merupakan jenis pupuk yang dapat dibuat seseorang dengan memanfaatkan sampah yang rumah tangga yang didaur ulang, seperti sampah makanan atau sampah organik lainnya.</p>
+                <div class="d-flex justify-content-between align-items-center">
+                  <div class="btn-group">
+                    <a class="btn btn-outline-dark btn-sm" href="https://www.kompas.com/homey/read/2021/07/06/080600276/cara-membuat-kompos-dari-sampah-rumah-tangga-tanpa-bau?page=all" target="_blank" role="button">Read</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+    </div>
+    </div>
+</div>
+
+<!--Artikel end-->
+`;
 
 export default homeTemplate;
