efe7b78163c715e4b85a8fcf34854f08b8b5d898
ardanu01 <ardanubroto@gmail.com>
2023-06-18 20:50:29 +0800

    update service.js

--- src/scripts/views/pages/service.js
+++ src/scripts/views/pages/service.js
@@ -118,17 +118,6 @@ const serviceWebsite = {
       const samapahFormContainer = document.querySelector('.tukarPage__form_2');
       const check = document.getElementById('checkbox');
       const next = document.getElementById('next');
-      
-        if (check.checked = true) {
-          next.removeAttribute('disabled');
-          next.addEventListener('click', (event) => {
-            event.preventDefault();
-            formBiodataContainer.setAttribute('id', 'hiddenForm');
-            samapahFormContainer.removeAttribute('id');
-          });
-        } else if (check.checked = false){
-          next.setAttribute('disabled');
-        }
 
         const nama = document.getElementById('nama').value;
         const alamat = document.getElementById('alamat').value;
@@ -140,10 +129,22 @@ const serviceWebsite = {
         if (nama == '' || alamat == '' || namaBank == '' || noRekening == '' || notelpon == '' || email == ''){
           alert('Mohon Isi Data Dengan Benar');
           const check = document.getElementById('checkbox');
-          check.checked = false;
-          button.disabled = true;
+          check.disabled = true;
+          next.disabled = true;
         }
       
+        if (check.checked = true) {
+          next.removeAttribute('disabled');
+          next.addEventListener('click', (event) => {
+            event.preventDefault();
+            formBiodataContainer.setAttribute('id', 'hiddenForm');
+            samapahFormContainer.removeAttribute('id');
+            button.disabled = true;
+          });
+        } else if (check.checked = false){
+          next.setAttribute('disabled');
+          button.disabled =  true;
+        }
 
       const jenis = document.getElementById('jenissampah');
       jenis.addEventListener('change', (event) => {
